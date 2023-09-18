45e0259a94bba69a479c3d4eeb12ac18b2fbe585
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-09-18 10:40:16 +0900

    Add frozen_string_literal comment to Book model
    - Added `# frozen_string_literal: true` to the top of the Book model to enforce string immutability.
    - This ensures all string literals in the file are immutable by default, potentially improving performance and preventing unintended string mutations.

--- Chapter-2/book_admin/app/models/book.rb
+++ Chapter-2/book_admin/app/models/book.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
+# This class represents a book entity.
 class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
